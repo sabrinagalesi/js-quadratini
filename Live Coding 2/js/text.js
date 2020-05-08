e830b94af74769b685793e38914010629529e539
sabrinagalesi <sabrinagalesi8@gmail.com>
2020-05-08 19:19:36 +0200

    Iniziato Live Coding 2

--- Live Coding 2/js/text.js
+++ Live Coding 2/js/text.js
@@ -1,1 +1,9 @@
 /* Creare una piccola sezione FAQ. Tante domande che quando cliccate visualizzano la risposta corrispondente. */
+
+$(document).ready(function(){
+    $(".list").each(function(){
+        $(this).click(function(){
+            $("#contenitor-sottotitolo").slideToggle();
+        })
+    })
+})
